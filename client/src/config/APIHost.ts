6b8b1f265264d222d98b2e07b5de6aabcf592d52
nozomi-iida <iida19990106@gmail.com>
2021-10-25 10:28:11 +0900

    fix: api host

--- client/src/config/APIHost.ts
+++ client/src/config/APIHost.ts
@@ -1,7 +1,7 @@
 export const APIHost = ((): string => {
   switch (process.env.REACT_APP_ENV) {
     case "production":
-      return "https://api.prob-works.com";
+      return "https://api-slack-react-ruby.herokuapp.com";
     default:
       // モックに接続
       return "https://9d29243fe462.ngrok.io";
