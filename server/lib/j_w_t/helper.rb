6190d699b14ed1cdc51ae54cfb839376e025127a
nozomi-iida <iida19990106@gmail.com>
2021-10-26 01:29:47 +0900

    fix: jwt helper

--- server/lib/j_w_t/helper.rb
+++ server/lib/j_w_t/helper.rb
@@ -5,11 +5,11 @@ module JWT
     module_function
 
     def encode(payload, alg = 'HS256', typ = 'JWT', **opts)
-      JWT.encode(payload, Rails.application.secrets.secret_key_base, alg, typ: typ, **opts)
+      JWT.encode(payload, Rails.application.credentials.secret_key_base, alg, typ: typ, **opts)
     end
 
     def decode(token, verify: true, **opts)
-      JWT.decode(token, Rails.application.secrets.secret_key_base, verify, **opts)
+      JWT.decode(token, Rails.application.credentials.secret_key_base, verify, **opts)
     end
   end
 end
